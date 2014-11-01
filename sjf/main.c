26de7637aa123586f47a9108652916c013ca6c53
Aniket Mitra <aniketmitra@Anikets-MacBook-Air-2.local>
2014-11-01 09:49:57 +0530

    Initial Commit

---
+++ sjf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sjf
+//
+//  Created by Aniket Mitra on 01/11/14.
+//  Copyright (c) 2014 Aniket Mitra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
